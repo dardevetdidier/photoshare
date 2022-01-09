ff15657be46ff6b408356aee95e31a2e057fb02d
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-09 18:00:37 +0100

    Create category and photo models Add static files create photo template

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories,
+               'photos': photos}
+    return render(request, 'photos/gallery.html', context=context)
 
 
 def viewphoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(pk=pk)
+    context = {'photo': photo}
+    return render(request, 'photos/photo.html', context=context)
 
 
 def addphoto(request):
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-Fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -120,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
