5f9b3954ec7de3cebdc961796c81576672581107
Didier Dardevet <didier.dardevet@gmail.com>
2022-01-09 15:51:52 +0100

    create gallery template with bootstrap Add Nav and gallery

--- photos/urls.py
+++ photos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gallery', views.gallery, name="gallery"),
-    path('photo/<str:pk>/', views.viewphoto, name="view"),
+    path('', views.gallery, name="gallery"),
+    path('photo/<str:pk>/', views.viewphoto, name="photo"),
     path('add/', views.addphoto, name="add"),
 ]
